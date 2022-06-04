8f03f0d3bb2e95e3071d03081db5634b29d8889a
huyiheng <3091296503@qq.com>
2022-06-04 17:31:11 +0800

    新增了难度选择

---
+++ src/com/game/EliteObj.java
@@ -0,0 +1,86 @@
+package com.game;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Rectangle;
+
+public class EliteObj extends GameObj{
+	
+	int life=5;
+	int originlife=5;
+	int dead=0;
+	@Override
+	public void paintSelf(Graphics g) {
+		// TODO Auto-generated method stub
+		super.paintSelf(g);
+		y += speed;
+		if(this.getRec().intersects(this.frame.planeobj.getRec())){
+			if(GameWindow.mode==4||GameWindow.mode==3) {
+				PlaneObj.life-=3;
+				ExplodeObj explodeObj = new ExplodeObj(x,y);
+				GameUtils.explodeObjList.add(explodeObj);
+				GameUtils.removeObjList.add(explodeObj);
+				this.x=-200;
+				this.y=200;
+				GameUtils.removeObjList.add(this);
+			}
+			else GameWindow.state = 3;
+		}
+		for(ShellObj shellObj:GameUtils.shellObjList) {
+			if(this.getRec().intersects(shellObj.getRec())) {
+				life-=PlaneObj.atk;
+				shellObj.setX(-100);
+				shellObj.setY(100);
+				GameUtils.removeObjList.add(shellObj);
+			}
+			if(life<=0) {
+				ExplodeObj explodeObj = new ExplodeObj(x,y);
+				GameUtils.explodeObjList.add(explodeObj);
+				GameUtils.removeObjList.add(explodeObj);
+				this.x=-200;
+				this.y=200;
+				if(dead==0) {
+					GameWindow.score+=5;
+					GameWindow.money+=5;
+					dead=1;
+				}
+				GameUtils.removeObjList.add(this);
+			}
+		}
+		g.setColor(Color.white);
+		g.fillRect(this.x+10,this.y+50,50,10);
+		g.setColor(Color.red);
+		g.fillRect(this.x+10,this.y+50,life*50/originlife,10);
+		if(y>600) {
+			if(life>0&&(GameWindow.mode==4||GameWindow.mode==3)) {
+				PlaneObj.life-=2;
+			}
+			this.x=-200;
+			this.y=200;
+			GameUtils.removeObjList.add(this);
+		}
+	}
+
+	@Override
+	public Rectangle getRec() {
+		// TODO Auto-generated method stub
+		return super.getRec();
+	}
+	
+	public EliteObj(Image img, int x, int y, int width, int height, double speed, GameWindow frame) {
+		super(img, x, y, width, height, speed, frame);
+	}
+	
+	public EliteObj(Image img, int x, int y, int width, int height, double speed, GameWindow frame,int life) {
+		this.img = img;
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.speed = speed;
+		this.frame = frame;
+		this.life = life;
+		this.originlife = life;
+	}
+}
---
+++ src/com/game/PlaneObj.java
@@ -0,0 +1,48 @@
+package com.game;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class PlaneObj extends GameObj{
+	
+	public static int life = 10;
+	
+	public static int atk = 1;
+
+	@Override
+	public void paintSelf(Graphics g) {
+		// TODO Auto-generated method stub
+		super.paintSelf(g);
+		if(this.frame.bossobj!=null&&this.getRec().intersects(this.frame.bossobj.getRec())) {
+			GameWindow.state = 3;
+		}
+		if(life<=0) {
+			GameWindow.state=3;
+		}
+	}
+
+	@Override
+	public Rectangle getRec() {
+		// TODO Auto-generated method stub
+		return super.getRec();
+	}
+
+	public Image getImg() {
+		return super.getImg();
+	}
+	
+	public PlaneObj() {
+		super();
+	}
+	
+	public PlaneObj(Image img, int x, int y, int width, int height, double speed, GameWindow frame) {
+		super(img, x, y, width, height, speed, frame);
+		this.frame.addMouseMotionListener(new MouseAdapter() {
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				PlaneObj.super.x=e.getX()-11;
+				PlaneObj.super.y=e.getY()-16;
+			}
+		});
+	}
+}
